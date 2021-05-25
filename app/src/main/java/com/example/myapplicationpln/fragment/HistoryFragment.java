344dd4db421a5abda4f7c15fa30b61b78232eabd
narumiya1 <narumiiyamei@gmail.com>
2021-05-25 11:10:08 +0700

    Historyi Page

---
+++ app/src/main/java/com/example/myapplicationpln/fragment/HistoryFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapplicationpln.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplicationpln.R;
+
+public class HistoryFragment extends Fragment {
+    public HistoryFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_historyi, container, false);
+    }
+}
