4564a88849976e250e0fcfeb7a6eae6c9563a974
narumiya1 <narumiiyamei@gmail.com>
2021-06-17 08:56:18 +0700

    GmeterValue add x & y

--- app/src/main/java/com/example/myapplicationpln/roomDb/GCameraValue.java
+++ app/src/main/java/com/example/myapplicationpln/roomDb/GCameraValue.java
@@ -22,7 +22,10 @@ public class GCameraValue implements Serializable {
     public int height;
     @ColumnInfo(name = "type")
     public int type;
-
+    @ColumnInfo(name = "x")
+    public int x;
+    @ColumnInfo(name = "y")
+    public int y;
     public int getId() {
         return id;
     }
@@ -69,5 +72,21 @@ public class GCameraValue implements Serializable {
 
     public void setType(int type) {
         this.type = type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
