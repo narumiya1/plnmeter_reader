bb3f24d953713bd336a14715fedc1c744a6bb55b
narumiya1 <narumiiyamei@gmail.com>
2021-06-18 11:25:18 +0700

    Check connection , check if selection null , db room integration , get data gUserData.getPhone .

--- app/src/main/java/com/example/myapplicationpln/fragment/UserDataFragment.java
+++ app/src/main/java/com/example/myapplicationpln/fragment/UserDataFragment.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.room.Room;
 
 import com.example.myapplicationpln.R;
 import com.example.myapplicationpln.activities.AddressListActivity2;
@@ -25,6 +26,9 @@ import com.example.myapplicationpln.model.Connection;
 import com.example.myapplicationpln.model.Toastr;
 import com.example.myapplicationpln.preference.SessionPrefference;
 import com.example.myapplicationpln.model.DataUser;
+import com.example.myapplicationpln.roomDb.AppDatabase;
+import com.example.myapplicationpln.roomDb.GUserData;
+import com.example.myapplicationpln.roomDb.Ghistoryi;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,8 +36,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class UserDataFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class UserDataFragment extends Fragment {
+    private AppDatabase db;
+    private ArrayList<GUserData> listuser;
     String name, getUserIdfSession, adress, numberPhone;
     TextView tvUserName, tvEmail, tvAddress, tvPhone, tv_realname, tv_id_pel;
     TextView tvUserEdit;
@@ -50,7 +59,12 @@ public class UserDataFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.example_prof, container, false);
-
+        db = Room.databaseBuilder(getContext(), AppDatabase.class, "tbGrainHistory")
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .addMigrations(AppDatabase.MIGRATION_1_6)
+                .build();
+        listuser = new ArrayList<>();
         tvUserName = rootView.findViewById(R.id.tvUser);
         tv_realname = rootView.findViewById(R.id.tv_realname);
         tvPhone = rootView.findViewById(R.id.tv_account_phone);
@@ -108,46 +122,74 @@ public class UserDataFragment extends Fragment {
          one to many
         firebase.database().ref('works').orderBy('customerKey').equalTo('customerKey2')
         */
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        Query query = reference.child("User").child(String.valueOf(numberPhone));
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    Log.d("DATA CHANGEt", "onDataChange: " + dataSnapshot.getValue());
-                    dataUser = dataSnapshot.getValue(DataUser.class);
-                    Log.d("DATA CHANGE detail", "onDataChange: " + dataUser.getAddress());
-                    tvUserName.setText(dataUser.getUsername());
-                    tv_realname.setText(dataUser.getName());
-                    tvEmail.setText(dataUser.getEmail());
-                    tvPhone.setText(dataUser.getPhone());
-                    tvAddress.setText(dataUser.getAddress());
-                    tv_id_pel.setText(dataUser.getId_pelanggan());
+        if (Connection.isConnect(getActivity())) {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child("User").child(String.valueOf(numberPhone));
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        Log.d("DATA CHANGEt", "onDataChange: " + dataSnapshot.getValue());
+                        dataUser = dataSnapshot.getValue(DataUser.class);
+                        Log.d("DATA CHANGE detail", "onDataChange: " + dataUser.getAddress());
+                        tvUserName.setText(dataUser.getUsername());
+                        tv_realname.setText(dataUser.getName());
+                        tvEmail.setText(dataUser.getEmail());
+                        tvPhone.setText(dataUser.getPhone());
+                        tvAddress.setText(dataUser.getAddress());
+                        tv_id_pel.setText(dataUser.getId_pelanggan());
 
-                    String id_pel = dataUser.getId_user();
-                    session.setUserId(id_pel);
-                    tvUserEdit.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
+                        String id_pel = dataUser.getId_user();
+                        session.setUserId(id_pel);
+                        tvUserEdit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
 //                selectImage();
-                            Intent intent = new Intent(getActivity(), UpdateUserData.class);
-                            intent.putExtra("userEmail", dataUser.getEmail());
-                            intent.putExtra("userName", dataUser.getUsername());
-                            intent.putExtra("userPhone", dataUser.getPhone());
+                                Intent intent = new Intent(getActivity(), UpdateUserData.class);
+                                intent.putExtra("userEmail", dataUser.getEmail());
+                                intent.putExtra("userName", dataUser.getUsername());
+                                intent.putExtra("userPhone", dataUser.getPhone());
 
-                            startActivity(intent);
-                        }
-                    });
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
                 }
+            });
+        }else {
+            String selection = db.gHistorySpinnerDao().readDataUser() ;
+            Log.d("selectionDb  ","name "+selection +" ? ");
+
+            if (selection!=null){
+                int iduser = Integer.parseInt(session.getUserId()) ;
+                List<GUserData> selectionu = Arrays.asList(db.gHistorySpinnerDao().selectDataUser());
+                GUserData gUserData = db.gHistorySpinnerDao().selectDetailUserData(session.getUserId());
+                Log.d("selectionlistuser  ","name "+selectionu +" ? ");
+                Log.d("selectionsx  ","name "+selection);
+                Log.d("gUserData  ","gUserData "+gUserData.getUsername());
+                tvUserName.setText(selection);
+                tv_realname.setText(String.valueOf(gUserData.getUsername()));
+                tvPhone.setText(String.valueOf(gUserData.getPhone()));
+                tvEmail.setText(String.valueOf(gUserData.getEmail()));
+                tvAddress.setText(String.valueOf(gUserData.getAddress()));
+                tv_id_pel.setText(String.valueOf(gUserData.getId_pel()));
+                tvUserEdit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toastr.showToast(getActivity(), "Cant edited, turn on connection");
+                    }
+                });
+            }else {
+                Toastr.showToast(getActivity(), "no connection");
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
+        }
         return rootView;
     }
 
